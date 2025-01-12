c51d564b7011c2b0ac2322ea8f20321114b3f9f7
Fábio Signorini <95833161+FabioSigF@users.noreply.github.com>
2025-01-12 11:25:05 -0300

    React Window performe optimization on ProductsTable

--- frontend/src/components/ProductsTable.tsx
+++ frontend/src/components/ProductsTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../redux/store";
 import {
@@ -6,6 +6,10 @@ import {
   openEditProductModal,
   openRemoveProductModal,
 } from "../redux/modal/slice";
+
+// React Window - Performance optimization
+import { FixedSizeList} from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 const ProductsTable = ({ products }) => {
   const dispatch: AppDispatch = useDispatch();
@@ -22,6 +26,38 @@ const ProductsTable = ({ products }) => {
     dispatch(openRemoveProductModal(id));
   };
 
+  // Renderiza cada linha da tabela
+  const renderRow = ({ index, style }) => {
+    const product = products[index];
+    return (
+      <div
+        style={style}
+        className="flex flex-row items-center justify-between border px-4 py-2 min-w-full"
+      >
+        <div className="w-full sm:w-1/3 py-1 text-left break-words">
+          {product.name}
+        </div>
+        <div className="w-full sm:w-1/3 py-1 text-left">
+          R$ {product.price.toFixed(2)}
+        </div>
+        <div className="w-full sm:w-1/3 flex gap-2 justify-center">
+          <button
+            onClick={() => handleEdit(product.id)}
+            className="mb-2 sm:mb-0 px-3 py-1 bg-yellow-500 text-white rounded hover:bg-yellow-600"
+          >
+            Editar
+          </button>
+          <button
+            onClick={() => handleRemove(product.id)}
+            className="mb-2 sm:mb-0 px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700"
+          >
+            Remover
+          </button>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="p-8">
       <h1 className="text-2xl font-bold mb-6">
@@ -34,39 +70,27 @@ const ProductsTable = ({ products }) => {
         Adicionar Produto
       </button>
       <div className="overflow-x-auto">
-        <table className="min-w-full border border-gray-300 rounded-lg">
-          <thead>
-            <tr className="bg-gray-100">
-              <th className="border px-4 py-2 text-left">Nome</th>
-              <th className="border px-4 py-2 text-left">Preço</th>
-              <th className="border px-4 py-2 text-center">Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr key={product.id} className="odd:bg-white even:bg-gray-50">
-                <td className="border px-4 py-2">{product.name}</td>
-                <td className="border px-4 py-2">
-                  R$ {product.price.toFixed(2)}
-                </td>
-                <td className="border px-4 py-2 text-center flex flex-col sm:flex-row items-center gap-2 justify-center">
-                  <button
-                    onClick={() => handleEdit(product.id)}
-                    className="px-3 py-1  bg-yellow-500 text-white rounded hover:bg-yellow-600"
-                  >
-                    Editar
-                  </button>
-                  <button
-                    onClick={() => handleRemove(product.id)}
-                    className="px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700"
-                  >
-                    Remover
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div className="min-w-[540px] border border-gray-300 rounded-lg">
+          <div className="bg-gray-100 flex">
+            <div className="w-1/3 border px-4 py-2 text-left">Nome</div>
+            <div className="w-1/3 border px-4 py-2 text-left">Preço</div>
+            <div className="w-1/3 border px-4 py-2 text-center">Ações</div>
+          </div>
+          <div className="w-full h-[50vh]">
+            <AutoSizer>
+              {({ height, width }) => (
+                <FixedSizeList
+                  height={height}
+                  itemCount={products.length}
+                  itemSize={56}
+                  width={width}
+                >
+                  {renderRow}
+                </FixedSizeList>
+              )}
+            </AutoSizer>
+          </div>
+        </div>
       </div>
     </div>
   );
